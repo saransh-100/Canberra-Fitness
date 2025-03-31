af9f09278ccedd922450c9c682775f3663dea67e
saransh-100 <saranshkharel100@gmail.com>
2025-03-31 14:44:49 +1100

    Canberra fitness website

---
+++ script.js
@@ -0,0 +1,21 @@
+var countDownDate = new Date("August 11, 2025 15:27:25").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+    
+  var distance = countDownDate - now;
+    
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  if (distance < 0) {
+    clearInterval(x);
+    document.querySelector(".deal").innerHTML = `Sorry, the deal has <span style="color: rgb(227, 74, 56);">EXPIRED</span>`;
+  }
+}, 1000);
